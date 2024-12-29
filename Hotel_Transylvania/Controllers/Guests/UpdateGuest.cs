73d811b092dea75cd5ef6b4142b56417d8bf4b9f
niklashog <hogstromniklas@gmail.com>
2024-12-29 10:38:47 +0100

    Error handling with Spectre and Regex on UpdateGuest.cs. Done.

--- Hotel_Transylvania/Controllers/Guests/UpdateGuest.cs
+++ Hotel_Transylvania/Controllers/Guests/UpdateGuest.cs
@@ -5,6 +5,9 @@ using Hotel_Transylvania.Services;
 using Hotel_Transylvania.Interfaces.ServicesInterfaces;
 using Hotel_Transylvania.Data;
 using System.Text.RegularExpressions;
+using Spectre.Console;
+using System.Net;
+using System.Numerics;
 
 namespace Hotel_Transylvania.Menus.Guests
 {
@@ -21,25 +24,34 @@ namespace Hotel_Transylvania.Menus.Guests
             guestService.DisplayActiveGuests(dbContext);
 
             Console.CursorVisible = true;
-            Console.WriteLine("Update guest details. Leaving a field blank will keep current data.");
-                        
-            int guestToUpdate;
-            string guestToUpdatePattern = "^[0-9]+$";
-            while (true)
-            {
-                Console.Write("Guest Id: ");
-                var guestToUpdateInput = Console.ReadLine();
 
-                if (Regex.IsMatch(guestToUpdateInput, guestToUpdatePattern))
-                {
-                    guestToUpdate = int.Parse(guestToUpdateInput);
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Only digits are allowed. Try again.");
-                }
-            }
+            var activeGuests = guestService.ListOfActiveGuests(dbContext);
+            var validGuestIds = activeGuests
+                .Select(g => g.Id)
+                .ToList();
+
+            Console.CursorVisible = true;
+            AnsiConsole.MarkupLine("[bold yellow]Update Guest Details[/]");
+
+            string guestToUpdateInput = AnsiConsole.Prompt(
+                new TextPrompt<string>("Input [yellow]Guest to update: [/]:")
+                    .ValidationErrorMessage("[red]Please enter an existing Guest Id[/]")
+                    .Validate(input =>
+                    {
+                        if (!int.TryParse(input, out int guestId))
+                        {
+                            return ValidationResult.Error("[red]Guest Id has to be a number[/]");
+                        }
+
+                        if (!validGuestIds.Contains(guestId))
+                        {
+                            return ValidationResult.Error("[red]Guest Id doesn't exist[/]");
+                        }
+
+                        return ValidationResult.Success();
+                    })
+                    );
+            var guestToUpdate = int.Parse(guestToUpdateInput);
 
             Console.CursorVisible = false;
 
@@ -48,106 +60,113 @@ namespace Hotel_Transylvania.Menus.Guests
 
             guestService.PrintGuestDetails(guestToUpdate, dbContext);
 
-            string firstName;
-            string firstNamePattern = @"^[\p{L}]+$";
-            while (true)
-            {
-                Console.Write("\nNew First Name: ");
-                var firstNameInput = Console.ReadLine();
+            string firstNameInput = AnsiConsole.Prompt(
+                new TextPrompt<string>("Input [yellow]First Name[/]:")
+                    .ValidationErrorMessage("[red]First Names can only consist of letters.[/]")
+                    .Validate(input =>
+                    {
+                        if (Regex.IsMatch(input, @"^[\p{L}]{2,20}$"))
+                        {
+                            return ValidationResult.Success();
+                        }
+                        else
+                        {
+                            return ValidationResult.Error("[red]Names cannot include numbers or special characters and can only be 20 characters long. Try again.[/]");
+                        }
+                    })
+                    );
 
-                if (Regex.IsMatch(firstNameInput, firstNamePattern))
-                {
+            string surnameInput = AnsiConsole.Prompt(
+                new TextPrompt<string>("Input [yellow]Surname[/]:")
+                    .ValidationErrorMessage("[red]Surnames can only consist of letters.[/]")
+                    .Validate(input =>
+                    {
+                        if (Regex.IsMatch(input, @"^[\p{L}]{2,50}$"))
+                        {
+                            return ValidationResult.Success();
+                        }
+                        else
+                        {
+                            return ValidationResult.Error("[red]Surnames cannot include numbers or special characters and can only be 50 characters long. Try again.[/]");
+                        }
+                    })
+                    );
 
-                    firstName = firstNameInput;
-                    break;
-                }
-                else
-                {
-                    Console.CursorVisible = false;
-                    Console.WriteLine("Names cannot include numbers or special characters. Try again.");
-                }
-            }
+            string emailInput = AnsiConsole.Prompt(
+                new TextPrompt<string>("Input [yellow]E-mail Adress (required)[/]:")
+                    .ValidationErrorMessage("[red]Please enter a correct e-mail adress[/]")
+                    .Validate(input =>
+                    {
+                        if (Regex.IsMatch(input, @"^[a-zA-Z0-9._%+\-]{2,}@[a-zA-Z0-9.\-]{2,}\.[a-zA-Z]{2,}$"))
+                        {
+                            return ValidationResult.Success();
+                        }
+                        else
+                        {
+                            return ValidationResult.Error("[red]Each part of the e-mail adress must be at least 2 characters long (e.g. bo@ek.se). Try again.[/]");
+                        }
+                    })
+                    );
 
-            string surname;
-            string surnamePattern = @"^[\p{L}]+$";
-            while (true)
-            {
-                Console.Write("New Surname: ");
-                var surnameInput = Console.ReadLine();
-
-                if (Regex.IsMatch(surnameInput, surnamePattern))
-                {
-                    surname = surnameInput;
-                    break;
-                }
-                else
-                {
-                    Console.CursorVisible = false;
-                    Console.WriteLine("Names cannot include numbers or special characters. Try again.");
-                }
-            }
-
-
-            string email;
-            string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
-            while (true)
-            {
-                Console.Write("New E-mail: ");
-                var emailInput = Console.ReadLine();
-
-                if (Regex.IsMatch(emailInput, emailPattern))
-                {
-                    email = emailInput;
-                    break;
-                }
-                else
-                {
-                    Console.CursorVisible = false;
-                    Console.WriteLine("Enter a valid e-mail adress (required).");
-                }
-            }
-
-            string phone = null;
-            string phonePattern = @"^\+{0,2}\d+(-?\d+)*(\(\d+\))?$";
-            while (true)
-            {
-                Console.Write("New Phone number: ");
-                var phoneInput = Console.ReadLine();
-
-                if (string.IsNullOrWhiteSpace(phoneInput))
-                {
-                    phoneInput = null;
-                    Console.WriteLine("Phone number left blank.");
-                    break;
-                }
-
-                if (Regex.IsMatch(phoneInput, phonePattern))
-                {
-                    phone = phoneInput;
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Enter a valid phone number or leave blank.");
-                }
-            }
-
+            string phoneInput = AnsiConsole.Prompt(
+                new TextPrompt<string>("Input [yellow]Phone Number[/] (optional):")
+                    .ValidationErrorMessage("[red]Please enter a correct phone number[/]")
+                    .Validate(input =>
+                    {
+                        if (string.IsNullOrWhiteSpace(input))
+                        {
+                            phoneInput = null;
+                            Console.WriteLine("Phone number left blank.");
+                            return ValidationResult.Success();
+                        }
+                        if (Regex.IsMatch(input, @"^(?=.{1,20}$)\+{0,1}\d+(-?\d+)*(\(\d+\))?$"))
+                        {
+                            return ValidationResult.Success();
+                        }
+                        else
+                        {
+                            return ValidationResult.Error("[red]Enter a valid phone number or leave blank. Try again.[/]");
+                        }
+                    })
+                    );
             Console.CursorVisible = false;
-            Console.Write("\nPress 'Enter' to save..");
-            Console.ReadKey();
+            Console.Clear();
+            DisplayLogo.Paint();
 
             var currentGuestDetails = guestService.GetGuestById(guestToUpdate, dbContext);
 
-
             var updatedGuestDetails = new string[]
-            {
-                firstName,
-                surname,
-                email,
-                phone ?? "---"
-            };
+{
+                firstNameInput,
+                surnameInput,
+                emailInput,
+                phoneInput ?? "---"
+};
+            AnsiConsole.MarkupLine("\n[bold yellow]Summary:[/]");
+            var table = new Table();
+            table.AddColumn("[yellow]Detail[/]");
+            table.AddColumn("[yellow]Current[/]");
+            table.AddColumn("[yellow]New[/]");
+            table.AddRow("First Name:", currentGuestDetails.FirstName, firstNameInput);
+            table.AddRow("Surname:", currentGuestDetails.Surname, surnameInput);
+            table.AddRow("E-mail:", currentGuestDetails.Email, emailInput);
+            table.AddRow("Phone:", currentGuestDetails.Phone, phoneInput);
+            AnsiConsole.Write(table);
 
-            guestService.UpdateGuestDetails(guestToUpdate, updatedGuestDetails, dbContext);
+            bool confirm = AnsiConsole.Confirm("\nConfirm new details are correct");
+
+            if (confirm)
+            {
+                AnsiConsole.MarkupLine("[bold green]Guest successfully edited![/]");
+                guestService.UpdateGuestDetails(guestToUpdate, updatedGuestDetails, dbContext);
+            }
+            else
+            {
+                AnsiConsole.MarkupLine("[bold red]Cancelled. Details remain unchanged.[/]");
+            }
+
+            Console.ReadKey();
+            
         }
     }
 }
