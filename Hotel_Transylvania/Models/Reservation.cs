89e8b484d91101ac8b8df2bfccccab81f565fbf3
niklashog <hogstromniklas@gmail.com>
2024-12-29 22:43:21 +0100

    Added .Date on Property TimeOfReservation in Reservation Model.

--- Hotel_Transylvania/Models/Reservation.cs
+++ Hotel_Transylvania/Models/Reservation.cs
@@ -10,7 +10,7 @@ namespace Hotel_Transylvania.Models
         public int NumberOfAdditionalBeds { get; set; }
         public DateTime CheckinDate { get; set; }
         public DateTime CheckoutDate { get; set; }
-        public DateTime TimeOfReservation { get; set; } = DateTime.Now;
+        public DateTime TimeOfReservation { get; set; } = DateTime.Now.Date;
         public bool IsReservationActive { get; set; } = true;
         public ICollection<Guest>? Guests { get; set; } = new List<Guest>();
         public ICollection<Room>? Rooms { get; set; } = new List<Room>();
