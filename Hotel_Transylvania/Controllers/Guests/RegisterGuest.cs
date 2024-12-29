1e4766a1cd89caedd3179ec6ce5d775cbd3a91f2
niklashog <hogstromniklas@gmail.com>
2024-12-29 10:02:49 +0100

    Implementation of Error handling with Spectre console. Kept regex as part of spectre validation. Birthinput written but awaiting full implementation before ucommenting.

--- Hotel_Transylvania/Controllers/Guests/RegisterGuest.cs
+++ Hotel_Transylvania/Controllers/Guests/RegisterGuest.cs
@@ -6,6 +6,7 @@ using Hotel_Transylvania.Services;
 using Hotel_Transylvania.Interfaces.ServicesInterfaces;
 using Hotel_Transylvania.Display;
 using System.Text.RegularExpressions;
+using Spectre.Console;
 
 
 namespace Hotel_Transylvania.Menus.Guests
@@ -20,106 +21,121 @@ namespace Hotel_Transylvania.Menus.Guests
 
             using var dbContext = ApplicationDbContext.GetDbContext();
 
+            string firstNameInput = AnsiConsole.Prompt(
+                new TextPrompt<string>("Input [yellow]First Name[/]:")
+                    .ValidationErrorMessage("[red]First Names can only consist of letters.[/]")
+                    .Validate(input =>
+                    {
+                        if (System.Text.RegularExpressions.Regex.IsMatch(input, @"^[\p{L}]{2,20}$"))
+                        {
+                            return ValidationResult.Success();
+                        }
+                        else
+                        {
+                            return ValidationResult.Error("[red]Names cannot include numbers or special characters and can only be 20 characters long. Try again.[/]");
+                        }
+                    })
+                    );
 
-            Console.CursorVisible = true;
-            Console.WriteLine("Enter guest details.");
+            string surnameInput = AnsiConsole.Prompt(
+                new TextPrompt<string>("Input [yellow]Surname[/]:")
+                    .ValidationErrorMessage("[red]Surnames can only consist of letters.[/]")
+                    .Validate(input =>
+                    {
+                        if (System.Text.RegularExpressions.Regex.IsMatch(input, @"^[\p{L}]{2,50}$"))
+                        {
+                            return ValidationResult.Success();
+                        }
+                        else
+                        {
+                            return ValidationResult.Error("[red]Surnames cannot include numbers or special characters and can only be 50 characters long. Try again.[/]");
+                        }
+                    })
+                    );
 
-            string firstName;
-            string firstNamePattern = @"^[\p{L}]+$";
-            while (true)
+            string emailInput = AnsiConsole.Prompt(
+                new TextPrompt<string>("Input [yellow]E-mail Adress[/]:")
+                    .ValidationErrorMessage("[red]Please enter a correct e-mail adress[/]")
+                    .Validate(input =>
+                    {
+                        if (System.Text.RegularExpressions.Regex.IsMatch(input, @"^[a-zA-Z0-9._%+\-]{2,}@[a-zA-Z0-9.\-]{2,}\.[a-zA-Z]{2,}$"))
+                        {
+                            return ValidationResult.Success();
+                        }
+                        else
+                        {
+                            return ValidationResult.Error("[red]Each part of the e-mail adress must be at least 2 characters long (e.g. bo@ek.se). Try again.[/]");
+                        }
+                    })
+                    );
+
+            string phoneInput = AnsiConsole.Prompt(
+                new TextPrompt<string>("Input [yellow]Phone Number[/]:")
+                    .ValidationErrorMessage("[red]Please enter a correct phone number[/]")
+                    .Validate(input =>
+                    {
+                        if (string.IsNullOrWhiteSpace(input))
+                        {
+                            phoneInput = null;
+                            Console.WriteLine("Phone number left blank.");
+                            return ValidationResult.Success();
+                        }
+                        if (System.Text.RegularExpressions.Regex.IsMatch(input, @"^(?=.{1,20}$)\+{0,1}\d+(-?\d+)*(\(\d+\))?$"))
+                        {
+                            return ValidationResult.Success();
+                        }
+                        else
+                        {
+                            return ValidationResult.Error("[red]Enter a valid phone number or leave blank. Try again.[/]");
+                        }
+                    })
+                    );
+
+            //Slår på denna när jag har implementerat date of birth i Guest
+
+            //string birthInput = AnsiConsole.Prompt(
+            //    new TextPrompt<string>("Input [yellow]date of birth (yyyy-mm-dd)[/]:")
+            //        .ValidationErrorMessage("[red]You can only input digits.[/]")
+            //        .Validate(input =>
+            //        {
+            //            return DateTime.TryParseExact(
+            //                input,
+            //                "yyyy-MM-dd",
+            //                System.Globalization.CultureInfo.InvariantCulture,
+            //                System.Globalization.DateTimeStyles.None,
+            //                out _)
+            //                ? ValidationResult.Success()
+            //                : ValidationResult.Error("[red]Please input date of birth as yyyy-mm-dd. Try again.[/]");
+            //        })
+            //        );
+
+
+            bool confirm = AnsiConsole.Confirm("\nIs the above information correct?");
+
+            if (confirm)
             {
-                Console.Write("First Name: ");
-                var firstNameInput = Console.ReadLine();
+                AnsiConsole.MarkupLine("[green]Registration successful.[/]");
 
-                if (Regex.IsMatch(firstNameInput, firstNamePattern))
+                var newGuest = new Guest
                 {
-                    firstName = firstNameInput;
-                    break;
-                }
-                else
-                {
-                    Console.CursorVisible = false;
-                    Console.WriteLine("Names cannot include numbers or special characters. Try again.");
-                }
+                    FirstName = firstNameInput,
+                    Surname = surnameInput,
+                    Email = emailInput,
+                    Phone = phoneInput ?? "---"
+                };
+                guestService.AddGuest(newGuest, dbContext);
             }
-
-            string surname;
-            string surnamePattern = @"^[\p{L}]+$";
-            while (true)
+            else
             {
-                Console.Write("Surname: ");
-                var surnameInput = Console.ReadLine();
-
-                if (Regex.IsMatch(surnameInput, surnamePattern))
-                {
-                    surname = surnameInput;
-                    break;
-                }
-                else
-                {
-                    Console.CursorVisible = false;
-                    Console.WriteLine("Names cannot include numbers or special characters. Try again.");
-                }
-            }
-
-
-            string email;
-            string emailPattern = @"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$";
-            while (true)
-            {
-                Console.Write("E-mail: ");
-                var emailInput = Console.ReadLine();
-
-                if (Regex.IsMatch(emailInput, emailPattern))
-                {
-                    email = emailInput;
-                    break;
-                }
-                else
-                {
-                    Console.CursorVisible = false;
-                    Console.WriteLine("Enter a valid e-mail adress (required).");
-                }
-            }
-
-            string phone = null;
-            string phonePattern = @"^\+{0,2}\d+(-?\d+)*(\(\d+\))?$";
-            while (true)
-            {
-                Console.Write("Phone number: ");
-                var phoneInput = Console.ReadLine();
-
-                if (string.IsNullOrWhiteSpace(phoneInput))
-                {
-                    phoneInput = null;
-                    Console.WriteLine("Phone number left blank.");
-                    break;
-                }
-
-                if (Regex.IsMatch(phoneInput, phonePattern))
-                {
-                    phone = phoneInput;
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Enter a valid phone number or leave blank.");
-                }
+                AnsiConsole.MarkupLine("[red]Registration cancelled.[/]");
             }
 
             Console.CursorVisible = false;
-            Console.Write("\nPress 'Enter' to save..");
             Console.ReadKey();
 
-            var newGuest = new Guest
-            {
-                FirstName = firstName,
-                Surname = surname,
-                Email = email,
-                Phone = phone ?? "---"
-            };
 
-            guestService.AddGuest(newGuest, dbContext);
+
+
         }
     }
 }
